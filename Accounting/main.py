e7f27613f5b786ad12f7337705c5254bbd45fb7d
hsuan <hsuan@hsuan.app>
2022-07-19 21:11:20 +0800

    Added Books

--- Accounting/main.py
+++ Accounting/main.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 
 from . import db
+from .models import Book
 
 main = Blueprint('main', __name__)
 
@@ -15,3 +16,28 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', user=current_user)
+
+
+@main.route('/books')
+@login_required
+def books():
+    return render_template('books.html', user=current_user)
+
+
+@main.route('/add_book')
+@login_required
+def add_book():
+    return render_template('add_book.html')
+
+
+@main.route('/add_book', methods=["POST"])
+@login_required
+def add_book_post():
+    name = request.form['name']
+
+    new_book = Book(name=name, user_id=current_user.id)
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return redirect(url_for('main.books'))
--- Accounting/models.py
+++ Accounting/models.py
@@ -4,7 +4,17 @@ from . import db
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+
+    books = db.relationship('Book', backref='user')
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.Column(db.String(100))
